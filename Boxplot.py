f667b14c6d8e55e9a6160a13463613ceab987667
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-09 19:10:24 +0630

    new py added

---
+++ Boxplot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+# Import pandas as pd
+import pandas as pd
+
+mens=pd.read_csv("summer2016.csv")
+mens_rowing=mens[mens["Sport"]=="Rowing"]
+mens_gymnastics=mens[mens["Sport"]=="Gymnastics"]
+fig, ax = plt.subplots()
+
+# Add a boxplot for the "Height" column in the DataFrames
+ax.boxplot([mens_rowing['Height'],mens_gymnastics['Height']])
+
+# Add x-axis tick labels:
+ax.set_xticklabels(['Rowing','Gymnastics'])
+
+# Add a y-axis label
+ax.set_ylabel("Height (cm)")
+
+plt.show()
