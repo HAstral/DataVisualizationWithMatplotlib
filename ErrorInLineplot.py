7b637b733790a89accd00ef5ab4f2401e906c1c5
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-09 19:07:35 +0630

    new py added

---
+++ ErrorInLineplot.py
@@ -0,0 +1,20 @@
+# Import the matplotlib.pyplot submodule and name it plt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+seattle_weather=pd.read_csv("seattle_weather.csv")
+austin_weather=pd.read_csv("austin_weather.csv")
+seattle_weather["MONTH"] = seattle_weather["MONTH"].astype(str)
+austin_weather["MONTH"] = austin_weather["MONTH"].astype(str)
+fig, ax = plt.subplots()
+
+# Add Seattle temperature data in each month with error bars
+ax.errorbar(seattle_weather["MONTH"], seattle_weather["MLY-TAVG-NORMAL"], yerr=seattle_weather["MLY-TAVG-STDDEV"])
+
+# Add Austin temperature data in each month with error bars
+ax.errorbar(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"], yerr=austin_weather["MLY-TAVG-STDDEV"]) 
+
+# Set the y-axis label
+ax.set_ylabel("Temperature(Fahrenheit)")
+
+plt.show()
