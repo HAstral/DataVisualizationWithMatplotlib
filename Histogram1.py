213e117915a09073835ec96bbd0fdd5a47173041
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-09 18:28:03 +0630

    new py added

---
+++ Histogram1.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+# Import pandas as pd
+import pandas as pd
+
+mens=pd.read_csv("summer2016.csv")
+mens_rowing=mens[mens["Sport"]=="Rowing"]
+mens_gymnastics=mens[mens["Sport"]=="Gymnastics"]
+fig, ax = plt.subplots()
+# Plot a histogram of "Weight" for mens_rowing
+ax.hist(mens_rowing['Weight'])
+
+# Compare to histogram of "Weight" for mens_gymnastics
+ax.hist(mens_gymnastics["Weight"])
+
+# Set the x-axis label to "Weight (kg)"
+ax.set_xlabel("Weight (kg)")
+
+# Set the y-axis label to "# of observations"
+ax.set_ylabel("# of observations")
+
+plt.show()
